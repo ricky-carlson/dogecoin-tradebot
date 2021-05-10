cf57efabd1e404684eb8d8b8ea901ee321cb2630
Ricky Carlson <mastergaming1999@yahoo.com>
2021-05-10 13:28:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from datetime import datetime
 from functions import get_price
 
 performance_price = []
+time_list = []
 
-#Loop that gets 'live' market data every N minutes for X coin
+#Loop that gets market data every N minutes for X coin
 i = 0
 while i < 2:
-    performance_price.append(get_price("DOGE-USD"))
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    time_list.append(current_time)
+    performance_price.append(float(get_price("DOGE-USD")))
+    
     if len(performance_price) < 2:
         time.sleep(60)
         i += 1
     else:
         i += 1
-print(performance_price)
+print(performance_price, time_list)
