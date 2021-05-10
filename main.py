27a449b3a5756b7bfd0889e3eca5a78a7ad6d4e2
Ricky Carlson <mastergaming1999@yahoo.com>
2021-05-10 17:19:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import time
 from datetime import datetime
 from functions import get_price
 
-#Match performance_price with time_list to create time stamp
 performance_price = []
-time_list = []
+time_price = []
+price_change = {}
 
 #Loop that gets market data every N minutes for X coin
 i = 0
-while i < 5:
+while i < 2:
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
 
-    time_list.append(current_time)
+    time_price.append(current_time)
     performance_price.append(float(get_price("DOGE-USD")))
-    
-    if len(performance_price) < 5:
+
+    if len(performance_price) < 2:
         time.sleep(60)
         i += 1
     else:
         i += 1
-print(performance_price, time_list)
+print(performance_price, time_price)
+
+#Calculates change of Nth + 1 price from Nth price 
+for i in range(len(performance_price)):
+    if (i + 1) < len(performance_price):
+        price_change[round(performance_price[i + 1] - performance_price[i], 5)] = time_price[i]
+    print("Not enough data to calculate change!")
+print(price_change)
