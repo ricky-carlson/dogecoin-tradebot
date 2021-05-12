3ec5ca930a304dcd50536b1484d98092d4b5b9d6
mmmeaney <46251831+mmmeaney@users.noreply.github.com>
2021-05-12 13:34:30 -0500

    Start changes on my branch
    Small changes, need to test to make sure nothing breaks

--- main.py
+++ main.py
@@ -6,29 +6,31 @@ from functions import get_price
 performance_price = []
 time_price = []
 price_change = {}
-
+loopNum = 5
 time_interval = '1m'
 
 #Loop that gets market data every N minutes for X coin
 i = 0
-while i < 5:
+while i < loopNum:
+    
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-
     time_price.append(current_time)
     performance_price.append(float(get_price("DOGE-USD")))
-
-    if len(performance_price) < 5:
-        time.sleep(60)
-        i += 1
-    else:
-        i += 1
+    
+    time.sleep(60) 
+    i += 1
 
 #Calculates % change of Nth + 1 price from Nth price
-for x in range(len(performance_price)):
-    if (x + 1) < len(performance_price):
-        price_change[round((performance_price[x + 1] - performance_price[x])/performance_price[x], 5)] = time_interval
+for price in range(len(performance_price) -1):
+    if (price + 1) =< len(performance_price):
+        price_change[round((performance_price[price + 1] - performance_price[price])/performance_price[price], 5)]
     else:
-        print("Not enough data to calculate change!")
+        print("Finished Calculations")
+        
+        
+
 print("Process complete!")
-print(np.average(performance_price))
+print("Average Price: " + np.average(performance_price))
+print("Price Changes " + price_change)
+
