e3024dea643f8688b0deee882713be5ce9e0cb41
Ricky Carlson <mastergaming1999@yahoo.com>
2021-05-17 22:10:33 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -11,6 +11,15 @@ def get_price(coin):
 
     return(round(coin_price, 5))
 
+#Historical Market Data
+#Valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+#Valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+def historic_data(ticker, period, interval):
+    ticker = yf.download(ticker, period=period, interval=interval)
+    ticker_price = ticker['Close']
+
+    return(round(ticker_price, 2))
+
 #Gets market data every N minutes for X coin
 def market_data(price, current_time, loop):
     i = 0
