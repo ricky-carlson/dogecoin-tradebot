49014b0e42d7293918c6491e4751226e1bc9c410
Ricky Carlson <mastergaming1999@yahoo.com>
2021-05-19 11:50:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functions import get_price, market_data, price_change, historic_data, price_chart
+from functions import get_price, live_data, price_change, historic_data, price_chart
 
 performance_price = []
 time_price = []
