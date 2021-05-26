a520c47bef9c6cf4e7346879a4c45bbe9b3e39c2
Ricky Carlson <mastergaming1999@yahoo.com>
2021-05-26 11:48:03 -0500

    Update trade_strategy.py

--- trade_strategy.py
+++ trade_strategy.py
@@ -1,19 +1,18 @@
 import yfinance as yf
+import math
 from functions import get_price, historic_data, price_change, chart_price
 
-ticker = "sq".upper()
 
-stock_price = get_price(ticker)
 stop_loss = .025 #as percent
 entry_percent_limit = .15 #as percent
-trade_size = .05 #Percent of total capital per trade
 
-account_capital = 1000 #total trading capital available 
+#Trading Account Parameters
+account_capital = 1000 #total trading capital available
+trade_size = .20 #Percent of total capital per trade
 capital_per_trade = round(account_capital * trade_size, 0) #Amount of capital used per trade
-shares_owned = 2 #quantity of shares owned
 
-print(capital_per_trade)
-
+ticker = "fubo".upper()
+stock_price = get_price(ticker)
 i = False
 while i == False:
     try:
@@ -27,16 +26,21 @@ if (entry_price > stock_price * (1 + entry_percent_limit)) or (entry_price < sto
     print("Not a valid entry point!")
     quit()
 
+#Stop loss & Target Price numbers
 target_price = round(entry_price * (1 + .2), 2)
 stop_loss_price = round(entry_price * (1 - stop_loss), 2)
 
+#Purchasable shares within trade parameters
+if (entry_price > capital_per_trade):
+    print("Not enough account capital to make trade!")
+    quit()
+shares_owned = math.floor((capital_per_trade / entry_price))
+
+#Trade requirements & loss/profit
 capital_required = round(entry_price * shares_owned, 2)
 max_loss = round((stop_loss_price - entry_price) * shares_owned, 2)
 profit = round((target_price - entry_price) * shares_owned, 2)
 
 print(f"\nStock: {ticker}\nCapital required: ${capital_required}\nStop loss price: ${stop_loss_price}\nTarget price: ${target_price}\n\nMax loss: ${max_loss}\nProfit at Target Price: ${profit}")
 
-
-chart_price(historic_data(ticker, "ytd", "5d"), target_price, stop_loss_price)
-
-
+#chart_price(historic_data(ticker, "ytd", "5d"), target_price, stop_loss_price)
