cdd856d7168b3a1f2e520a320ec6c0a7d16ba12f
Ricky Carlson <mastergaming1999@yahoo.com>
2021-05-26 10:54:14 -0500

    Update trade_strategy.py

--- trade_strategy.py
+++ trade_strategy.py
@@ -1,14 +1,13 @@
-import matplotlib.pyplot as plt
 import yfinance as yf
-from functions import get_price
+from functions import get_price, historic_data, price_change, chart_price
 
-ticker = "fubo".upper()
+ticker = "sq".upper()
 
 stock_price = get_price(ticker)
 stop_loss = .025 #as percent
 entry_percent_limit = .15 #as percent
 
-shares_owned = 20
+shares_owned = 2 #quantity of shares owned
 
 i = False
 while i == False:
@@ -26,4 +25,12 @@ if (entry_price > stock_price * (1 + entry_percent_limit)) or (entry_price < sto
 target_price = round(entry_price * (1 + .2), 2)
 stop_loss_price = round(entry_price * (1 - stop_loss), 2)
 
-print(f"\n{ticker} stop loss price: ${stop_loss_price}\n{ticker} target price: ${target_price}")
+capital_required = round(entry_price * shares_owned, 2)
+max_loss = round((stop_loss_price - entry_price) * shares_owned, 2)
+profit = round((target_price - entry_price) * shares_owned, 2)
+
+print(f"\nStock: {ticker}\nCapital required: ${capital_required}\nStop loss price: ${stop_loss_price}\nTarget price: ${target_price}\n\nMax loss: ${max_loss}\nProfit at Target Price: ${profit}")
+
+
+chart_price(historic_data(ticker, "ytd", "5d"), target_price, stop_loss_price)
+
